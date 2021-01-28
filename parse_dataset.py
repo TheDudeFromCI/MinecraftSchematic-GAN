53455e3b683f84ca1600f7d957145dfffd8eea64
TheDudeFromCI <thedudefromci@gmail.com>
2021-01-28 06:50:40 -0700

    Seperated converting and downloading.

--- parse_dataset.py
+++ parse_dataset.py
@@ -2,29 +2,33 @@ import json
 import urllib.request
 from nbtschematic import SchematicFile
 import numpy as np
+from os import listdir as listdir
+from os import join as join
 
 
-def convert_dataset(url_file, output_folder, temp_folder, transforms):
+def download_dataset(url_file, output_folder):
     schematic_data = json.load(url_file)
 
     index = 0
     for schematic in schematic_data:
         url = schematic['downloadLink']
-        temp_file = temp_folder + '/' + str(index) + '.schematic'
-        urllib.request.urlretrieve(url, temp_file)
-        sf = SchematicFile.load(temp_file)
+        schematic_file = output_folder + '/' + str(index) + '.schematic'
+        urllib.request.urlretrieve(url, schematic_file)
+        index += 1
+
+
+def convert_dataset(schematic_folder, output_folder, transforms):
+    for file in listdir(schematic_folder):
+        schematic_file = join(schematic_folder, file)
+        output_file = join(output_folder, file.replace('schematic', '.npy'))
+
+        sf = SchematicFile.load(schematic_file)
 
         blocks = sf.blocks
         for transform in transforms:
             blocks = transform(blocks)
 
-        save_schematic_as_numpy(blocks, output_folder, index)
-        index += 1
-
-
-def save_schematic_as_numpy(blocks, output_folder, index):
-    file_name = output_folder + '/' + str(index) + '.npy'
-    np.save(file_name, blocks)
+        np.save(output_file, blocks)
 
 
 def array_to_schematic(blocks):
